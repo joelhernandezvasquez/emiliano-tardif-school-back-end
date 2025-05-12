1d6cb04aa634d89d33a5701d5587f63ead108bff
Joel Hernandez <jhernandez98@outlook.es>
2025-05-12 16:47:27 -0400

    add login endpoint

---
+++ src/presentation/services/auth.service.ts
@@ -0,0 +1,69 @@
+import { bcryptAdapter } from "../../config/bcrypt.adapter";
+import { JwtAdapter } from "../../config/jwt.adapter";
+import { prisma } from "../../data/postgres";
+import { CustomError } from "../../domain/errors/custom.error";
+
+
+interface User{
+    email:string,
+    password:string
+}
+
+export class AuthService{
+    constructor(
+    ){}
+
+    public async registerUser(registerUser:User){
+        const {email, password} = registerUser;
+        const existUser  = await prisma.users.findUnique({
+            where:{email}
+        })
+        if(existUser) throw CustomError.badRequest('Email already exist');
+   
+        try{
+            const user = await prisma.users.create({
+                data: {
+                 email,
+                 name: "Default Name",
+                 role:'admin',
+                 password_hash:bcryptAdapter.hash(password)
+                },
+              })
+   
+           return {user};
+        } 
+   
+        catch(error){
+          throw CustomError.internalServerError(`${error}`)
+        }
+       }
+
+
+    public async loginUser(loginUser:User){
+     const {email, password} = loginUser;
+     const user = await prisma.users.findUnique({
+        where:{email}
+     });
+
+    if(!user) throw CustomError.badRequest('Email not exist');
+
+    const isMatching = bcryptAdapter.compare(password,user.password_hash);
+
+     if(!isMatching) throw CustomError.badRequest('Password is invalid');
+
+     const{password_hash,...userEntity} = user;
+
+    const token = await JwtAdapter.generateToken({id:user.id});
+   
+
+    if(!token)
+    throw CustomError.internalServerError('Error while creating JWT')
+
+      return{
+        user:userEntity,
+        token
+      }
+    }
+
+   
+}
